be2cc0e99375e0ed4c8ea9e4d1edd5cb17efd46b
Molorane Mothusi <molorane.mothusi@gmail.com>
2021-04-04 15:04:19 +0200

    first commit

---
+++ src/main/java/org/dclm/sealRegistry/SealRegistryApplication.java
@@ -0,0 +1,13 @@
+package org.dclm.sealRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SealRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SealRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dclm/sealRegistry/SealRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dclm.sealRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
