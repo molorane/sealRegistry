dc84f3712fdc31bd55e27cf986bb7884e8a69d3f
Molorane Mothusi <molorane.mothusi@gmail.com>
2021-04-05 00:14:40 +0200

    update

--- src/main/java/org/dclm/sealRegistry/SealRegistryApplication.java
+++ src/main/java/org/dclm/sealRegistry/SealRegistryApplication.java
@@ -2,8 +2,10 @@ package org.dclm.sealRegistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SealRegistryApplication {
 
 	public static void main(String[] args) {
